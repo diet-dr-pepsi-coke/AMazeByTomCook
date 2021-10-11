58f6a4bb5896e07ea97481a5d66cefcd3464b24b
Tom Cook <tcook@email.wm.edu>
2021-10-11 13:07:40 -0400

    Finally created a working system to make a dictionary of wallboards that carry edge weights AND neighboring boards have the same weight.

--- MazeByTomCook/src/generation/MazeBuilderBoruvka.java
+++ MazeByTomCook/src/generation/MazeBuilderBoruvka.java
@@ -4,18 +4,20 @@ import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
+import generation.CardinalDirection;
 import generation.SingleRandom;
 
 public class MazeBuilderBoruvka extends MazeBuilder implements Runnable {
-	public SingleRandom random;
-	public Dictionary<Wallboard, Integer> boards;
+	public SingleRandom random = SingleRandom.getRandom();
+	public Dictionary<Wallboard, Integer> boards = new Hashtable<Wallboard, Integer>();
+	public ArrayList<Wallboard> walls = new ArrayList<Wallboard>();
 	
 	public MazeBuilderBoruvka() {
 		super();
 		System.out.println("MazeBuilderBoruvka uses Boruvka's algorithm to generate maze.");
 	}
 	
-	public void setEdgeWeight(int x, int y, Wallboard w) {
+	public void setEdgeWeight(Wallboard w) {
 		// This method first checks the wallboard to see if it is a 
 		// border using isPartofBorder() method from Floorplan.java.
 		// If it is not a border, then create an integer value "weight"
@@ -25,16 +27,19 @@ public class MazeBuilderBoruvka extends MazeBuilder implements Runnable {
 		// Then, go to the neighbor of this wallboard and set the weight of 
 		// the same board to the same integer value.
 		if (floorplan.isPartOfBorder(w)) {
+			System.out.println("Border wall included in internal wallboard list.");
 			return;
 		}
-		else {
-			int weight = random.nextInt();
-			boards.put(w, weight);	
-			CardinalDirection cd = w.getDirection();
-			int neighborX = w.getNeighborX();
-			int neighborY = w.getNeighborY();
-			
-			
+		if (boards.get(w) == -1) {
+			if (walls.indexOf(w) != walls.size() - 1) {
+			int weight = random.nextIntWithinInterval(0,100);
+			boards.put(w, weight);
+			for (Wallboard i : walls) {
+				if (w.getNeighborX() == i.getX() && w.getNeighborY() == i.getY() && w.getDirection().oppositeDirection() == i.getDirection()) {
+					boards.put(i, weight);
+				}
+			}
+			}
 		}	
 	}
 
@@ -54,7 +59,19 @@ public class MazeBuilderBoruvka extends MazeBuilder implements Runnable {
 		
 		floorplan.initialize();
 		createListOfInternalWallboards();
-		
+		//System.out.println(walls);
+		//System.out.println(boards);
+		for (Wallboard i : walls) { 
+		setEdgeWeight(i);
+		}
+		for (Wallboard i : walls) { 
+			int lob = getEdgeWeight(i);
+			System.out.println("x = " + i.getX());
+			System.out.println("y = " + i.getY());
+			System.out.println("cd = " + i.getDirection());
+			System.out.println("weight = " + lob);
+		}
+		System.out.println(boards);
 	}
 	/**
 	 * Updates a list of all wallboards that could be removed from the maze based on wallboards towards new cells.
@@ -67,7 +84,6 @@ public class MazeBuilderBoruvka extends MazeBuilder implements Runnable {
 	 * @param wallboards the new elements should be added to, must not be null
 	 */
 	private ArrayList<Wallboard> createListOfInternalWallboards() {
-		ArrayList<Wallboard> walls = new ArrayList<Wallboard>();
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y< height; y++) {
 		Wallboard wallboard = new Wallboard(x, y, CardinalDirection.East) ;
@@ -76,14 +92,37 @@ public class MazeBuilderBoruvka extends MazeBuilder implements Runnable {
 			if (!floorplan.isPartOfBorder(wallboard)) // 
 			{
 				walls.add(new Wallboard(x, y, cd));
+			//	walls.add(new Wallboard(wallboard.getNeighborX(),wallboard.getNeighborY(), cd.oppositeDirection()));
+				
 			}
 		}
 			}
 		}
 		for (Wallboard i : walls) {
-			System.out.println(i)	;
+			// initialize every wallboard to have a weight of -1
+			boards.put(i, -1);
 		}
 		return walls;
+	}
+	
+	public int wallboardInfo(Wallboard w) {
+		int x = w.getX();
+		int y = w.getY();
+		int neighborX = w.getNeighborX();
+		int neighborY = w.getNeighborY();
+		CardinalDirection cd = w.getDirection();
+		int intCD = 0;
+		switch (cd) {
+		case North:
+			intCD = 1;
+		case East:
+			intCD = 2;
+		case South:
+			intCD = 3;
+		case West:
+			intCD = 4;
+		}
+		return 0;
 	}
 }
 	
