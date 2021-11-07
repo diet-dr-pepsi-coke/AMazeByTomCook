32125b04ec42e78e8f07ac6411a02e941b0e1393
Tom Cook <tcook@email.wm.edu>
2021-11-07 14:47:40 -0500

    Refactored MazeFileReader to remove its color dependency

--- MazeByTomCook/src/gui/MazeFileReader.java
+++ MazeByTomCook/src/gui/MazeFileReader.java
@@ -12,7 +12,6 @@ import generation.Maze;
 import generation.MazeContainer;
 import generation.Wall;
 
-import java.awt.Color;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -216,7 +215,7 @@ public class MazeFileReader {
 		Wall result = new Wall(x,y,dx,dy,dist,cc) ;
 		// get a few more attributes and set those explicitly
 		int col = getElementIntValue("colSeg_" + number+ "_" + i, eElement);
-		result.setColor(new Color(col)); 
+		result.setColor(P5PanelF21.getWallColor(dist, cc, dx)); 
 		result.setSeen(getElementBooleanValue("seenSeg_" + number+ "_" + i, eElement));
 		result.setPartition(getElementBooleanValue("partitionSeg_" + number+ "_" + i, eElement));
 		return result;
